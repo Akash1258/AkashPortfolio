633f6b1aa0f07481a5472d2cd68f87eee3fcbf4e
Krakash06 <akash_fw11_290@masai.school>
2021-11-20 23:12:49 +0530

    img fix

--- src/PortfolioContainer/Home/Profile.jsx
+++ src/PortfolioContainer/Home/Profile.jsx
@@ -7,46 +7,45 @@ import './profile.css'
 
 export default function Profile() {
     return (
-            <div className="row" >
-                <div className="profile-container col-md-12" id="home">
-                    <div className="row ">
-                        <div className="col-md-12">
-                            
-                            <Navbar className="navBar-bg" expand={false} fixed="top">
-                                <Container fluid>
-                                    <Navbar.Brand ><h5 className="heading">Akash Kumar</h5></Navbar.Brand>
-                                    <Navbar.Toggle className="toggle-btn" aria-controls="offcanvasNavbar"
-                                        style={{ background: "#ffffff" }} />
-                                    <Navbar.Offcanvas
-                                        id="offcanvasNavbar"
-                                        aria-labelledby="offcanvasNavbarLabel"
-                                        placement="left"
-                                        className="menu-bar"
-                                    >
-                                        <Offcanvas.Header closeButton>
-                                            <Offcanvas.Title id="offcanvasNavbarLabel">
-                                                <div className="menubar-img">
-                                                    <img src=".././akash.png" alt="profile" />
-                                                </div>
-                                            </Offcanvas.Title>
-                                        </Offcanvas.Header>
-                                        <Offcanvas.Body>
-                                            <Nav>
-                                                <Nav.Link href="#home"><p className="menu-content">HOME</p></Nav.Link>
-                                                <Nav.Link href="#about"><p className="menu-content">ABOUT</p></Nav.Link>
-                                                <Nav.Link href="#resume"><p className="menu-content">RESUME</p></Nav.Link>
-                                                <Nav.Link href="#project"><p className="menu-content">PROJECTS</p></Nav.Link>
-                                                <Nav.Link href="#contact"><p className="menu-content">CONTACT</p></Nav.Link>
-                                            </Nav>
-                                        </Offcanvas.Body>
-                                    </Navbar.Offcanvas>
-                                </Container>
-                            </Navbar>
-                        </div>
+        <div className="row" >
+            <div className="profile-container col-md-12" id="home">
+                <div className="row ">
+                    <div className="col-md-12">
+
+                        <Navbar className="navBar-bg" expand={false} fixed="top">
+                            <Container fluid>
+                                <Navbar.Brand ><h5 className="heading">Akash Kumar</h5></Navbar.Brand>
+                                <Navbar.Toggle className="toggle-btn" aria-controls="offcanvasNavbar"
+                                    style={{ background: "#ffffff" }} />
+                                <Navbar.Offcanvas
+                                    id="offcanvasNavbar"
+                                    aria-labelledby="offcanvasNavbarLabel"
+                                    placement="left"
+                                    className="menu-bar"
+                                >
+                                    <Offcanvas.Header closeButton>
+                                        <Offcanvas.Title id="offcanvasNavbarLabel">
+                                            <div className="menubar-img">
+                                                <img src=".././akash.png" alt="profile" />
+                                            </div>
+                                        </Offcanvas.Title>
+                                    </Offcanvas.Header>
+                                    <Offcanvas.Body>
+                                        <Nav>
+                                            <Nav.Link href="#home"><p className="menu-content">HOME</p></Nav.Link>
+                                            <Nav.Link href="#about"><p className="menu-content">ABOUT</p></Nav.Link>
+                                            <Nav.Link href="#resume"><p className="menu-content">RESUME</p></Nav.Link>
+                                            <Nav.Link href="#project"><p className="menu-content">PROJECTS</p></Nav.Link>
+                                            <Nav.Link href="#contact"><p className="menu-content">CONTACT</p></Nav.Link>
+                                        </Nav>
+                                    </Offcanvas.Body>
+                                </Navbar.Offcanvas>
+                            </Container>
+                        </Navbar>
                     </div>
-                   <div className="row p-1">
-                       <div className="col-md-12 ">
-                       <div className="profile-parent mt-5 mb-5 p-5">
+                </div>
+                <div className="row p-1">
+                    <div className="profile-parent col-md-12 mt-5 mb-5 p-5 ">
                         <div className="profile-details">
                             <div className="colz">
                                 <div className="colz-icon">
@@ -80,10 +79,7 @@ export default function Profile() {
                                 </span>
                             </div>
                             <div className='profile-option'>
-                                {/* <button className="btn-primary-btn">
-                                {" "}
-                                Hire Me{" "}
-                            </button> */}
+
                                 <a href="resume.pdf" download='resume.pdf'>
                                     <button className="btn highlighted-btn">Get Resume</button>
                                 </a>
@@ -95,9 +91,8 @@ export default function Profile() {
                             </div>
                         </div>
                     </div>
-                       </div>
-                   </div>
                 </div>
             </div>
+        </div>
     );
 }
