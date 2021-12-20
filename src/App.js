e8db3ba78beff52791dfa57eca48805ec0f2a853
Krakash06 <akash_fw11_290@masai.school>
2021-12-20 23:44:06 +0530

    resume btn

--- src/App.js
+++ src/App.js
@@ -4,33 +4,31 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import AboutMe from './PortfolioContainer/AboutMe/AboutMe';
 import Resume from './PortfolioContainer/Resume/Resume';
 import Contact from './PortfolioContainer/ContactMe/Contact'
-import {Project} from './PortfolioContainer/Projects/Project'
+import { Project } from './PortfolioContainer/Projects/Project'
 import Card from 'react-bootstrap/Card'
 import Toggle from './PortfolioContainer/toggle/Toggle';
 import { useContext } from 'react';
 import { ThemeContext } from './context';
-
 
 function App() {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
   return (
     <>
-    <div className="App container-fluid"
-           style={{
-            backgroundColor: darkMode ? "#1F1301" : "#F6F4E6",
-            color: darkMode && "whitesmoke",
-          }}>
-      <Toggle/>
-     <Profile/>
-     <AboutMe/>
-     <Resume/>
-     <Project/>
-     <Contact/>
-     <Card body className="footer">© 2021 Akash Kumar
-
-</Card>
-    </div>
+      <div className="App container-fluid"
+        style={{
+          backgroundColor: darkMode ? "#1F1301" : "#F6F4E6",
+          color: darkMode && "whitesmoke",
+        }}>
+        <Toggle />
+        <Profile />
+        <AboutMe />
+        <Resume />
+        <Project />
+        <Contact />
+        <Card body className="footer">© 2021 Akash Kumar
+        </Card>
+      </div>
     </>
   );
 }
