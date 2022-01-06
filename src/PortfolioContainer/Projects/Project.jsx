5695e6aa6422e24839e5fa4dfaad2536cbe3dc90
Krakash06 <akash_fw11_290@masai.school>
2022-01-06 16:32:08 +0530

    Thrid project added

--- src/PortfolioContainer/Projects/Project.jsx
+++ src/PortfolioContainer/Projects/Project.jsx
@@ -12,8 +12,8 @@ export const Project = () => {
                         <p>Lybrate is an online
                             healthcare platform for any kind of medical assistance.</p>
                         <div className="links">
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/abhishek-hero/lybrate_Clone"><FaGithubSquare className='social-media-icon' /></a>
-                        <a target="_blank"  rel="noopener noreferrer" href="https://lybrateclone01.herokuapp.com"><FaLink className='social-media-icon' /></a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/abhishek-hero/lybrate_Clone"><FaGithubSquare className='social-media-icon' /></a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://lybrateclone01.herokuapp.com"><FaLink className='social-media-icon' /></a>
                         </div>
                         <div className="langDiv">
                             <p className="techstack">Tech Stack :</p>
@@ -54,6 +54,32 @@ export const Project = () => {
                     </div>
                     <div className="projectImg secProjectImg">
                         <img src="https://user-images.githubusercontent.com/77065070/141605993-0099f02f-82d2-416e-a8de-6eab80f12ca9.png" alt="" />
+                    </div>
+                </div>
+                <div className="projectCardDiv secProject">
+                    <div className="projectDetail">
+                        <h3>Zostel Clone</h3>
+                        <p>Zostel is one such one-size-fits-all kind of backpacker's hostel chain in
+                            India. As compared to the closed and isolated rooms of a hotel, in
+                            Zostel you will get to share dorms with strangers from other corners of
+                            the world.</p>
+                        <div className="links">
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/kerrybli/zostel-clone"><FaGithubSquare className='social-media-icon' /></a>
+                            <a target="_blank" rel="noopener noreferrer" href="https://zostel-clone-kerrybli.vercel.app/"><FaLink className='social-media-icon' /></a>
+                        </div>
+                        <div className="langDiv">
+                            <p className="techstack">Tech Stack :</p>
+                            <div className="texttackDiv">
+                                <img src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="" />
+                                <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white" alt="" />
+                                <img src="https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white" alt="" />
+                                <img src="https://img.shields.io/badge/bootstrap-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB" alt="" />
+
+                            </div>
+                        </div>
+                    </div>
+                    <div className="projectImg">
+                        <img src="https://user-images.githubusercontent.com/88572701/148372593-fe50b68d-ce05-4d6a-83de-6333d63a1796.png" alt="" />
                     </div>
                 </div>
             </div>
